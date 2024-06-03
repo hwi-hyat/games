59c9efe5d0dedc0a5cdcf0b3961c124861454391
hwi-hyat <prespektrejp@gmail.com>
2024-06-03 18:39:58 +0900

    little changes

--- src/snake.h
+++ src/snake.h
@@ -7,11 +7,20 @@ class snake
     private:
         //window
 
-
+        unsigned int board_size_x;
+        unsigned int board_size_y;
 
     public:
         snake();
+        snake(unsigned int board_size_x, unsigned int board_size_y);
         ~snake();
 
+        //setter
+        void set_board_size_x(unsigned int board_size_x);
+        void set_board_size_y(unsigned int board_size_y);
+
+        //getter
+        unsigned int get_board_size_x() const;
+        unsigned int get_board_size_y() const;
         
 }
